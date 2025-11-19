41eda2bbb7721790301555f0d02be6350b2b0379
Sri Kidambi <1702865+kidambisrinivas@users.noreply.github.com>
2025-11-19 13:54:20 +0000

    Update PostWallets endpoint with wallet deployment params (#17)
    * Update PostWallets endpoint with wallet deployment params
    
    * Updated byte size for RSA public keys
    
    * Get OpenAPI spec function for dependent services to add validation
    
    * Added discriminator

--- generate.go
+++ generate.go
@@ -1,5 +1,5 @@
 package main
 
-//go:generate go tool oapi-codegen -config ginserver/oapi-cfg.yaml openapi.yaml
-//go:generate go tool oapi-codegen -config stdserver/oapi-cfg.yaml openapi.yaml
-//go:generate go tool oapi-codegen -config client/oapi-cfg.yaml openapi.yaml
+//go:generate go tool oapi-codegen -config ginserver/oapi-cfg.yaml ./spec/openapi.yaml
+//go:generate go tool oapi-codegen -config stdserver/oapi-cfg.yaml ./spec/openapi.yaml
+//go:generate go tool oapi-codegen -config client/oapi-cfg.yaml ./spec/openapi.yaml
